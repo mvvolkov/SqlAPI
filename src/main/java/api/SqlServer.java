78372a9f9095de264d60602ef1c1d9219f02d7c7
mvolkov <mvvolkov@mail.ru>
2019-11-08 17:56:16 +0300

    next iteration

--- src/main/java/api/SqlServer.java
+++ src/main/java/api/SqlServer.java
@@ -7,15 +7,14 @@ import java.util.List;
 
 public interface SqlServer {
 
-    void executeStatement(SqlStatement statement) throws SqlException;
-
     void createDatabase(String dbName) throws DatabaseAlreadyExistsException;
 
     void openDatabaseWithTables(String dbName, List<TableMetadata> tables);
 
     void persistDatabase(String dbName);
 
+    void executeStatement(SqlStatement statement) throws SqlException;
 
     @NotNull
-    ResultSet select(SelectExpression selectExpression) throws WrongValueTypeException, NoSuchTableException, NoSuchDatabaseException, NoSuchColumnException;
+    ResultSet select(SelectExpression selectExpression) throws SqlException;
 }
